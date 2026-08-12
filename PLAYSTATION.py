acbaa21394bcb4f5bda32e9ad6b6852a0ff00418
kelsarata2-create <kelsarata2@gmail.com>
2026-08-12 18:14:10 -0300

    Update PLAYSTATION.py

--- PLAYSTATION.py
+++ PLAYSTATION.py
@@ -6,6 +6,11 @@ import os
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
+def notificar(mensagem):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    resposta = requests.post(url, data={"chat_id": CHAT_ID, "text": mensagem})
+    print(resposta.status_code, resposta.text)
+
 
 def notificar(mensagem):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
@@ -33,6 +38,7 @@ with sync_playwright() as p:
             sleep(2)
             preco = pagina.get_by_text("R$").first
             novo_preco = float(preco.inner_text().replace('R$', '').replace(',', '.').strip())
+            print(f'{games}: R$ {novo_preco}')
             if novo_preco < 250:
                 mensagem = f'O preço de {games} esta {novo_preco} na Playstation store, vai comprar?'
                 notificar(mensagem)
